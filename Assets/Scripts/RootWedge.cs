8c6db903e1b863dc92e4d3b43bb5f84a0c2fb4d9
Dave Hardee <dhardee@gmail.com>
2014-09-23 14:22:37 -0600

    This is my starting point.

---
+++ Assets/Scripts/RootWedge.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshRenderer))]
+[RequireComponent (typeof(MeshCollider))]
+[RequireComponent (typeof(MeshFilter))]
+
+public class RootWedge : Wedge 
+{
+
+	protected MeshRenderer meshRenderer;
+	protected MeshCollider meshCollider;
+	protected MeshFilter meshFilter;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/Wedge.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof(MeshRenderer))]
+[RequireComponent (typeof(MeshCollider))]
+[RequireComponent (typeof(MeshFilter))]
+
+public class Wedge {
+	
+	public Wedge[] childWedges;
+	public Wedge rootWedge;
+	public Mesh visualMesh;
+	public static float DEPTH = (Mathf.Sqrt(3)/2);
+	public enum solid {filled, empty, mixed};
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
